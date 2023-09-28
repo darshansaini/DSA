eca11b263c8ab1799bda78c99b98d6c938b843e3
darshansaini <darshansaini683@gmail.com>
2023-09-28 20:16:48 +0530

    first file

---
+++ Main.java
@@ -0,0 +1,21 @@
+import java.util.HashMap;
+public class Main
+{
+	public static void main(String[] args) {
+	    
+	    HashMap<String, String> capitalCities = new HashMap<String, String>();
+	    capitalCities.put("England", "London");
+	    capitalCities.put("India", "Pune");
+	    capitalCities.put("USA", "LA");
+	    capitalCities.put("Norway", "Oslo");
+	    capitalCities.put("Germany", "Berlin");
+	    capitalCities.put("Japan", "Tokyo");
+	    System.out.println(capitalCities);
+	    System.out.println(capitalCities.size());
+	    
+		for(String i : capitalCities.values())
+	    {
+	        System.out.println(i);
+	    }
+	}
+}
